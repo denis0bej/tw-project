9a97d22b7f137fe50b4b9a689e6e197a8a61b66a
denis0bej <bejan.topse.denis@gmail.com>
2024-11-23 22:58:07 +0200

    am bagat animatie la navbar :)

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ function animateBox() {
     const box = document.querySelector('.box');
     const buttoncontainer = document.querySelector('.navbar-list');
     const headerh = document.querySelector('.navbar').children[0];
-    console.log(headerh)
     box.classList.toggle('animate');
     
     for(let i=0;i<3;i++){
-        if(buttoncontainer.children[i].className ===""){
-            buttoncontainer.children[i].className+="responsive";
+        if(buttoncontainer.children[i].className ==="1"){
+            buttoncontainer.children[i].className+=" responsive";
             //headerh.style.display = "none";
             buttoncontainer.style.width = "100%";
+            buttoncontainer.children[i].classList.toggle('animate_list');
+            setTimeout(() => { buttoncontainer.children[i].classList.toggle('animate_list')}, 0.1);
         } else {
-            buttoncontainer.children[i].className ="";
+            buttoncontainer.children[i].className ="1";
             //headerh.style.display = "inline-flex";
             buttoncontainer.style.width = "60px";
         }
